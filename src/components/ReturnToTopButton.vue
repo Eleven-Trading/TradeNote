2646bc5cf136be9a48f8ec3f5ba067e0f1c43f81
El Even <>
2024-02-11 14:41:06 +0100

    Modified css return to top button

--- src/components/ReturnToTopButton.vue
+++ src/components/ReturnToTopButton.vue
@@ -6,6 +6,6 @@
 
 <template>
     <button class="btn blueBtn btn-sm" id="btn-return-to-top" v-show="windowIsScrolled" v-on:click="returnToTop">
-        <i class="uil uil-arrow-up me-2"></i>Return to top
+        <i class="uil uil-arrow-up"></i>
     </button>
 </template>
